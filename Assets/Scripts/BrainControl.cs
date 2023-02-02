ae25dde4e40e9a6709b79a4101deb53e3dd35501
Brani <branislav.uher@student.ukf.sk>
2023-02-02 02:54:25 +0100

    brain movement

--- Assets/Scripts/BrainControl.cs
+++ Assets/Scripts/BrainControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,34 +10,65 @@ using UnityEngine;
 public class BrainControl : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float initialSpeed = 1;
-
-    public float speedRampUp = 1;
+    
+    private float cooldownTimeCntr = 0;
 
     public float currentSpeed;
 
-    public float spawnRadius;
+    private List<Vector2> possibleDirections = new List<Vector2>();
+    private bool dontResetToMiddle = false;
 
-    public Transform pivot;
+    public float startingBrainCooldown = 1;
+    public float cooldownReductionRate = 0.2f;
+    private float currentBrainCooldown;
+
+
+    public Transform originPosition;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        currentSpeed = initialSpeed;
-
-        float directionAngle = (Random.Range(1, 5) + 0.5f) * Mathf.PI /2;
-        // rb.velocity = Random.insideUnitCircle.normalized * currentSpeed;
-        rb.velocity = new Vector2(Mathf.Cos(directionAngle)*spawnRadius, Mathf.Sin(directionAngle)*spawnRadius ).normalized * currentSpeed;
-
-        // nastavenie zaciatocnej pozicie
-        float angle = Random.Range(0.0f, 1.0f) * Mathf.PI * 2f;
-        Debug.Log(angle);
-        Vector2 newPos = new Vector2(Mathf.Cos(angle)*spawnRadius, Mathf.Sin(angle)*spawnRadius ) + (Vector2) pivot.position;
-        transform.position = newPos;
+        transform.position = originPosition.position;
+        currentBrainCooldown = startingBrainCooldown;
+        cooldownTimeCntr = currentBrainCooldown;
+        
+        // naplnenie moznych smerov
+        possibleDirections.Add(Vector2.up);
+        possibleDirections.Add(Vector2.right);
+        possibleDirections.Add(Vector2.down);
+        possibleDirections.Add(Vector2.left);
+        
+        // nastavenie zaciatocneho smeru
+        decideDirection();
     }
 
     void Update()
     {
-        currentSpeed += speedRampUp * Time.deltaTime;
-        rb.velocity = rb.velocity.normalized * currentSpeed;
+        if (currentBrainCooldown < 0.1f) currentBrainCooldown = 0.1f;
+
+        decideDirection();
+    }
+
+    private void decideDirection()
+    {
+        cooldownTimeCntr += Time.deltaTime;
+        
+        if (!dontResetToMiddle && Vector2.Distance(originPosition.position, transform.position) < 0.1)
+        {
+            transform.position = originPosition.position;
+            rb.velocity = Vector2.zero;
+            if (cooldownTimeCntr > currentBrainCooldown)
+            {
+                var newDirection = possibleDirections[Random.Range(0, 4)];
+                rb.velocity = newDirection * currentSpeed;
+                dontResetToMiddle = true;
+                cooldownTimeCntr = 0;
+                currentBrainCooldown -= cooldownReductionRate;
+            }
+        }
+
+        if (Vector2.Distance(originPosition.position, transform.position) > 0.1)
+        {
+            dontResetToMiddle = false;
+        }
     }
 }
